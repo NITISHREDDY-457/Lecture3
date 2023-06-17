7097141b0dc0065edccc2061c3be3429ed7b9e56
NITISHREDDY-457 <103503915+NITISHREDDY-457@users.noreply.github.com>
2023-06-17 11:37:49 +0530

    Create PrimitiveDataTypes.java

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,24 @@
+package coreJava;
+
+public class PrimitiveDT {
+	public static void main(String [] args) {
+		int x = 45;
+		int y = 63;
+		System.out.println("Value of x:"+x);
+		System.out.println("Value of y:"+y);
+		
+		byte b = (byte)172;
+		System.out.println(b);
+		
+		int i = b ;
+		System.out.println(i);
+		
+		float a = 10;
+		double d = 1230;
+		double d1 = ++d;
+		
+		double result = a + d1;
+		System.out.println(result);
+	}
+
+}
